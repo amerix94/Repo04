83373ff4f27f88d7ba7f88982682dc6bb427c5d4
amerix94 <77324494+amerix94@users.noreply.github.com>
2021-09-17 09:35:56 +0200

    Add files via upload

---
+++ ex10/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    var myFirstName = "Amer";
+    var mySecondName= "Begic";
+    var myHomeTown = "Sarajevo";
+
+    return {
+        myFirstName, 
+        mySecondName,
+        myHomeTown
+ };
+}
+    console.log(main());
+    module.exports = main;
